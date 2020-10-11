33f97e335f28e7de65aeefb6f07bd459492aa95c
audgster <ahs3028@gmail.com>
2020-10-11 17:50:34 -0400

    Adds string encoding; adds permutation tests

--- src/algos/string_test.go
+++ src/algos/string_test.go
@@ -3,18 +3,108 @@ package algos
 import "testing"
 
 type permutationTestCase struct {
-	a string
-	b string
+	a        string
+	b        string
 	expected bool
+	testcase string
 }
 
 func TestIsPermutation(t *testing.T) {
 	testcases := []permutationTestCase{
 		{
-			a: "abc",
-			b: "abc",
-			expected
-		}, // same string are permutations
-		"abc": "bca", // actual permutation
+			a:        "abc",
+			b:        "abc",
+			expected: true,
+			testcase: "Both strings are identical",
+		},
+		{
+			a:        "abc",
+			b:        "bca",
+			expected: true,
+			testcase: "Out of order permutation",
+		},
+		{
+			a:        "abc",
+			b:        "bcd",
+			expected: false,
+			testcase: "Same length string, different charset",
+		},
+		{
+			a:        "abc",
+			b:        "aabbcc",
+			expected: false,
+			testcase: "strings are not the same length",
+		},
+	}
+
+	for _, test := range testcases {
+		actual := IsPermutation(test.a, test.b)
+
+		if actual != test.expected {
+			t.Logf("For testcase %s with strings %s and %s: expected %t, got %t", test.testcase, test.a, test.b, test.expected, actual)
+			t.Fail()
+		}
+	}
+}
+
+type replaceSpacesTestCase struct {
+	input      string
+	truelength int
+	expected   string
+	testcase   string
+}
+
+var replaceSpacesTestcases []replaceSpacesTestCase = []replaceSpacesTestCase{
+	{
+		input:      "My String  ",
+		truelength: 9,
+		expected:   "My%20String",
+		testcase:   "Single space with trailing spaces",
+	},
+	{
+		input:      "My String meep    ",
+		truelength: 14,
+		expected:   "My%20String%20meep",
+		testcase:   "Multiple spaces with trailing spaces",
+	},
+	{
+		input:      "nospaces",
+		truelength: 8,
+		expected:   "nospaces",
+		testcase:   "No spaces",
+	},
+	{
+		input:      "         ",
+		truelength: 3,
+		expected:   "%20%20%20",
+		testcase:   "All spaces",
+	},
+	{
+		input:      "",
+		truelength: 0,
+		expected:   "",
+		testcase:   "Empty String",
+	},
+}
+
+func TestReplaceSpacesBuffer(t *testing.T) {
+	for _, test := range replaceSpacesTestcases {
+		actual := ReplaceSpacesBuffer(test.input, test.truelength)
+
+		if actual != test.expected {
+			t.Logf("For testcase %s with input %q with true length %d: expected %q, got %q", test.testcase, test.input, test.truelength, test.expected, actual)
+			t.Fail()
+		}
+	}
+}
+
+func TestReplaceSpacesInPlace(t *testing.T) {
+	for _, test := range replaceSpacesTestcases {
+		actual := ReplaceSpacesInPlace([]rune(test.input), test.truelength)
+
+		if actual != test.expected {
+			t.Logf("For testcase %s with input %q with true length %d: expected %q, got %q", test.testcase, test.input, test.truelength, test.expected, actual)
+			t.Fail()
+		}
 	}
 }
--- src/algos/strings.go
+++ src/algos/strings.go
@@ -1,15 +1,59 @@
 package algos
 
-func isPermutation(a string, b string) bool {
+import (
+	"strings"
+)
+
+// UniqueChars - Unique character detection based on ASCII or Extended ASCII.
+func UniqueChars(input string) bool {
+	chars := make([]bool, 256, 256)
+
+	for _, character := range input {
+		charvalue := int(character)
+
+		if chars[charvalue] {
+			return false
+		}
+		chars[charvalue] = true
+
+	}
+	return true
+}
+
+// UniqueCharsHashset - Uses a hashset to determine if string is made up of unique characters. Really works better for Unicode.
+func UniqueCharsHashset(input string) bool {
+	chars := make(map[rune]bool)
+
+	// Luckily golang's range over string implicitly does unicode decoding so we don't have to worry about interpreting bytes
+	for _, character := range input {
+		if chars[character] {
+			return false
+		}
+
+		chars[character] = true
+	}
+	return true
+}
+
+// UniqueCharsBrute - Brute force implementation of checking for unique characters in a string
+func UniqueCharsBrute(input string) bool {
+	for i, leftchar := range input[:len(input)-1] {
+		for _, rightchar := range input[i+1:] {
+			if leftchar == rightchar {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+// IsPermutation - Algorithm to determine if a string is a permutation of a second string
+func IsPermutation(a string, b string) bool {
 	// permutations have the multiset of characters
 	// The must be the same length, and all the characters in them must be the same
 	if len(a) != len(b) {
 		return false
 	}
-
-	// Approaches:
-	// - sort both strings. If equal, they are permutations
-	// - count characters in each string and ensure they have the same counts
 
 	var characters [256]int
 
@@ -27,4 +71,64 @@ func isPermutation(a string, b string) bool {
 	}
 
 	return true
+}
+
+// ReplaceSpacesBuffer - A serializer of spaces to %20, implemented using string buffer
+func ReplaceSpacesBuffer(input string, truelength int) string {
+	var builder strings.Builder
+
+	for _, character := range input {
+		if truelength == 0 {
+			break
+		}
+		if character == ' ' {
+			builder.WriteString("%20")
+		} else {
+			builder.WriteRune(character)
+		}
+		truelength--
+	}
+
+	return builder.String()
+}
+
+// ReplaceSpacesInPlace - A serializer of spaces to %20, implemented using in place array.
+func ReplaceSpacesInPlace(input []rune, truelength int) string {
+	// For each rune. We are working with rune array so each one is already a unicode codepoint
+	// Check if it's a space
+	// If it's a space, shift everything over 2x
+	// replace current and next 2 with %20
+
+	if len(input) == 0 {
+		return ""
+	}
+
+	for i := 0; i < truelength; i++ {
+		if truelength == len(input) {
+			break
+		}
+
+		if input[i] == ' ' {
+			shiftRight(&input, i, truelength-1, 2)
+
+			input[i] = '%'
+
+			i++
+			input[i] = '2'
+			truelength++
+
+			i++
+			input[i] = '0'
+			truelength++
+		}
+	}
+
+	return string(input)
+}
+
+// Shift everything to the right of start index by n. You can assume the array is the correct size
+func shiftRight(array *[]rune, startIndex int, endIndex int, n int) {
+	for i := endIndex; i > startIndex; i-- {
+		(*array)[i+n] = (*array)[i]
+	}
 }
